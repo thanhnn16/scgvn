6a70c2a4d20e45af9917088167eccfd164cca398
Miwth <thanhnnps28372@fpt.edu.vn>
2024-03-10 13:38:21 +0700

    Disable reset password

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -39,12 +39,12 @@
         @endif
 
         <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-                   href="{{ route('password.request') }}">
-                    {{ __('Đặt lại mật khẩu') }}
-                </a>
-            @endif
+{{--            @if (Route::has('password.request'))--}}
+{{--                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"--}}
+{{--                   href="{{ route('password.request') }}">--}}
+{{--                    {{ __('Đặt lại mật khẩu') }}--}}
+{{--                </a>--}}
+{{--            @endif--}}
 
             <x-primary-button class="ms-3">
                 {{ __('Đăng nhập') }}
