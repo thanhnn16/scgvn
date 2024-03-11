16a566faf2f636ea94c763f526182e9dc7beaec4
Miwth <thanhnnps28372@fpt.edu.vn>
2024-03-11 12:34:21 +0700

    Add catch error when import

--- app/Imports/EventsImport.php
+++ app/Imports/EventsImport.php
@@ -23,17 +23,10 @@ class EventsImport implements ToModel, WithHeadingRow
         }
 
         try {
-            $startDate = $row['ngay_bat_dau'];
-            $endDate = $row['ngay_ket_thuc'];
-
-            if (!is_a($startDate, 'DateTime') || !is_a($endDate, 'DateTime')) {
-                throw new Exception('startDate và endDate phải là định dạng ngày');
-            } else {
-                $startDate = Date::excelToDateTimeObject($row['ngay_bat_dau']);
-                $endDate = Date::excelToDateTimeObject($row['ngay_ket_thuc']);
-            }
+            $startDate = Date::excelToDateTimeObject($row['ngay_bat_dau']);
+            $endDate = Date::excelToDateTimeObject($row['ngay_ket_thuc']);
         } catch (Exception $e) {
-            throw new Exception('Vui lòng chuyển ngày bắt đầu và ngày kết thúc sang định dạng Date trong Excel');
+            throw new Exception('startDate và endDate phải là định dạng ngày');
         }
 
         if ($endDate < $startDate) {
