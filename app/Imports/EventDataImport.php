1adfadafd60d1cb8f7f65a81a7f0fd72659a1277
Miwth <thanhnnps28372@fpt.edu.vn>
2024-03-11 12:03:28 +0700

    Added things to check error

--- app/Imports/EventDataImport.php
+++ app/Imports/EventDataImport.php
@@ -3,16 +3,28 @@
 namespace App\Imports;
 
 use App\Models\EventAgency;
+use Exception;
 use Maatwebsite\Excel\Concerns\WithMultipleSheets;
 
 class EventDataImport implements WithMultipleSheets
 {
     public function sheets(): array
     {
-        return [
-            'su_kien' => new EventsImport(),
-            'phan_thuong' => new PrizesImport(),
-            'dai_ly_tham_gia' => new EventAgenciesImport(),
-        ];
+//        return [
+//            'su_kien' => new EventsImport(),
+//            'phan_thuong' => new PrizesImport(),
+//            'dai_ly_tham_gia' => new EventAgenciesImport(),
+//        ];
+        try {
+            return [
+                'su_kien' => new EventsImport(),
+                'dai_ly_tham_gia' => new EventAgenciesImport(),
+                'phan_thuong' => new PrizesImport(),
+            ];
+        } catch (Exception $e) {
+            return [
+                'error' => $e->getMessage()
+            ];
+        }
     }
 }
